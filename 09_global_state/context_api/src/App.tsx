f6582a7fb20b056ce6d99449630eaa4be80f3466
larwis95 <156850505+larwis95@users.noreply.github.com>
2025-06-17 12:46:03 -0400

    global state finished

--- 09_global_state/context_api/src/App.tsx
+++ 09_global_state/context_api/src/App.tsx
@@ -1,14 +1,21 @@
 import CounterDisplay from "./components/CounterDisplay";
-import CounterControls from "./components/CounterControls";
+import CounterControls from "./components/CounterControl";
 import { CounterProvider } from "./context/CounterContext";
 
-export default function App() {
+function App() {
   return (
-    <div className="app-container">
-      {/* TODO: 5. Wrap CounterDisplay and CounterControls in the CounterProvider. */}
-      {/* These two components are siblings that need to share state. */}
-      <CounterDisplay />
-      <CounterControls />
+    <div className="main-container">
+      <h1>Counter App with Context API</h1>
+      <p>
+        Context can be used to pass state down deep component trees without prop
+        drilling, or even sharing data between sibling components.
+      </p>
+      <div className="app-container">
+        {/* TODO: 5. Wrap CounterDisplay and CounterControls in the CounterProvider. */}
+        {/* These two components are siblings that need to share state. */}
+        <CounterDisplay />
+        <CounterControls />
+      </div>
     </div>
   );
 }
--- 09_global_state/context_api/src/components/CounterControl.tsx
+++ 09_global_state/context_api/src/components/CounterControl.tsx
@@ -12,3 +12,5 @@ function CounterControls() {
     </div>
   );
 }
+
+export default CounterControls;
