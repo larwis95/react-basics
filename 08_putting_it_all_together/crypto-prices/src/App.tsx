2d7c53645e4a58e0409155d7c90beee31e4a7dc6
JHWisniewski <jhwisnie@umich.edu>
2025-06-22 19:27:02 -0400

    Initial app creation; modification of main page

---
+++ 08_putting_it_all_together/crypto-prices/src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <h1>Welcome to crypto-prices!</h1>
+      <p>You'll be able to search cryptocurrencies and learn more about them!</p>
+    </>
+  )
+}
+
+export default App
